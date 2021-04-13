f542a2a864fef625739438a1fa280c93e093592a
PopovichT <popovich@phystech.edu>
2021-04-13 19:59:35 +0300

    changes

--- src/main/java/com/popovich/scrap/ScraperService.java
+++ src/main/java/com/popovich/scrap/ScraperService.java
@@ -1,9 +1,13 @@
 package com.popovich.scrap;
 
 import lombok.AllArgsConstructor;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -13,9 +17,17 @@ public class ScraperService
 
     private final ChromeDriver driver;
 
+    @PostConstruct
+    void postConstruct()
+    {
+        scrape("auto");
+    }
+
     public void scrape (final String value) {
         driver.get(URL + "search/?text=" + value);
-        final WebElement words = driver.findElementByClassName("")
-
+        final WebElement item = driver.findElementByClassName("path organic_path");
+        final List<WebElement> itemList = item.findElements(By.tagName("a"));
+        itemList.forEach(scrapItem -> System.out.println(scrapItem.getText()));
+        driver.quit();
     }
 }
