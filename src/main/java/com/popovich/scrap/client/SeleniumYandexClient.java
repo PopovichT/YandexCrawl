514211dfac1389345ed028ae16e6a5e6b784f167
PopovichT <popovich@phystech.edu>
2021-04-14 19:17:48 +0300

    changes

--- src/main/java/com/popovich/scrap/client/SeleniumYandexClient.java
+++ src/main/java/com/popovich/scrap/client/SeleniumYandexClient.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service
@@ -17,15 +18,17 @@ public class SeleniumYandexClient {
         config = new SeleniumConfig();
         config.getDriver().get(URL);
     }
-//    public void search(String word) {
-//        config.getDriver().get(URL + "search/?text=" + word);
-//        List<WebElement> list = scrape();
+
+//    @PostConstruct
+//    void postConstruct()
+//    {
+//        List<WebElement> list = scrape("автомобиль");
+//        list.forEach(item -> System.out.println(item.getText()));
 //    }
 
     public List<WebElement> scrape(String word) {
         config.getDriver().get(URL + "search/?text=" + word);
-        final WebElement item = config.getDriver().findElement(By.className("path organic_path"));
-        final List<WebElement> itemList = item.findElements(By.tagName("a"));
+        final List<WebElement> itemList = config.getDriver().findElements(By.cssSelector("a[class*='organic__url']"));
         config.getDriver().quit();
         return itemList;
     }
